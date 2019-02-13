c2cf34da1828bde11367034227de076d6330d828
RomellPineda <romell.pineda@gmail.com>
2019-02-12 22:24:58 -0800

    bst v2

--- bst.js
+++ bst.js
@@ -1,45 +1,42 @@
-// My tree maker
+// My bst maker
 
 function Tree(data) {
     if (data < Number.POSITIVE_INFINITY) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
+      this.data = data;
+      this.left = null;
+      this.right = null;
     } else {
-        console.log('only accepting numbers right now');
+      console.log('only accepting numbers right now');
     }
-}
-
-Tree.prototype.insert = function (d) {
+  }
+  
+  Tree.prototype.insert = function (d) {
     if (d < Number.POSITIVE_INFINITY) {
-        if (d <= this.data) {
-            if (this.left == null) {
-                this.left == new Tree(d);
-            } else {
-                this.left.insert(d);
-            }
-        } else {
-            if (this.right == null) {
-                this.right == new Tree(d);
-            } else {
-                this.right.insert(d);
-            }
-        }
+      if (d <= this.data && this.left) {
+        this.left.insert(d);
+      } else if (d < this.data) {
+        this.left = new Tree(d);
+      } else if (d > this.data && this.right) {
+        this.right.insert(d);
+      } else if (d > this.data) {
+        this.right = new Tree(d);
+      }
     } else {
-        return 'just numbers, pal'
+      return 'just numbers, pal'
     }
-}
-
-Tree.prototype.contains = function (n) {
+  }
+  
+  Tree.prototype.contains = function (n) {
     if (n < Number.POSITIVE_INFINITY) {
-        if (n === this.data) {
-            return true;
-        } else if (n < this.data) {
-            console.log('I will look to the left');
-        } else {
-            console.log('I will look to the right');
-        }
+      if (n === this.data) {
+        return this;
+      } else if (n < this.data && this.left) {
+        return this.left.contains(n);
+      } else if (n > this.data && this.right) {
+        return this.right.contains(n);
+      }
+      return null;
     } else {
-        return 'this data structure only contains numbers';
+      return 'whaaaat!';
     }
-}
+  }
